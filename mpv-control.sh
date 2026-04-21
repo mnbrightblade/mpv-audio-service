2ba856de0d23633f29b054eebbfbbe57690f3d1f
mnbrightblade <chegney@gmail.com>
2026-04-21 15:36:22 -0500

    cleaning up mpv-control.sh script

--- mpv-control.sh
+++ mpv-control.sh
@@ -15,14 +15,6 @@ case "$1" in
     echo "Current: $current_title"
     echo "Next: $next_title"
     ;;   
-#status)
-#     playlist=$(echo '{ "command": ["get_property", "playlist"] }' | socat - "$socket" | jq -r '.data')
-#     current=$(echo "$playlist" | jq -r 'map(select(.current == true)) | first | .filename // "None"')
-#     next_index=$(echo "$playlist" | jq 'map(select(.current == true)) | first | .index // -1 | . + 1')
-#     next=$(echo "$playlist" | jq -r ".[$next_index].filename // \"None\"")
-#     echo "Current: $current"
-#     echo "Next: $next"
-#     ;;
   *) echo "Usage: $0 {play|pause|toggle|next|prev|status}" >&2; exit 1 ;;
 esac   
 
