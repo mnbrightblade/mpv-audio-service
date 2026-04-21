5605b389194e2bd1361e0ee8ec4d401885b59c90
mnbrightblade <chegney@gmail.com>
2026-04-21 15:09:24 -0500

    further updates to use metadata functionality

--- mpv-control.sh
+++ mpv-control.sh
@@ -7,13 +7,22 @@ case "$1" in
   toggle) echo '{ "command": ["cycle", "pause"] }' | socat - "$socket" ;;
   next)   echo '{ "command": ["playlist-next"] }' | socat - "$socket" ;;
   prev)   echo '{ "command": ["playlist-prev"] }' | socat - "$socket" ;;
-  status)
-      playlist=$(echo '{ "command": ["get_property", "playlist"] }' | socat - "$socket" | jq -r '.data')
-      current=$(echo "$playlist" | jq -r 'map(select(.current == true)) | first | .filename // "None"')
-      next_index=$(echo "$playlist" | jq 'map(select(.current == true)) | first | .index // -1 | . + 1')
-      next=$(echo "$playlist" | jq -r ".[$next_index].filename // \"None\"")
-      echo "Current: $current"
-      echo "Next: $next"
-      ;;
+ status)
+    playlist=$(echo '{ "command": ["get_property", "playlist"] }' | socat - "$socket")
+    current_title=$(echo "$playlist" | jq -r 'first(.data[] | select(.current == true)) | .title // .filename // "None"')
+    next_index=$(echo "$playlist" | jq 'first(.data[] | select(.current == true)) | .index // -1 | . + 1')
+    next_title=$(echo "$playlist" | jq -r ".data[$next_index].title // .data[$next_index].filename // \"None\"")
+    echo "Current: $current_title"
+    echo "Next: $next_title"
+    ;;   
+#status)
+#     playlist=$(echo '{ "command": ["get_property", "playlist"] }' | socat - "$socket" | jq -r '.data')
+#     current=$(echo "$playlist" | jq -r 'map(select(.current == true)) | first | .filename // "None"')
+#     next_index=$(echo "$playlist" | jq 'map(select(.current == true)) | first | .index // -1 | . + 1')
+#     next=$(echo "$playlist" | jq -r ".[$next_index].filename // \"None\"")
+#     echo "Current: $current"
+#     echo "Next: $next"
+#     ;;
   *) echo "Usage: $0 {play|pause|toggle|next|prev|status}" >&2; exit 1 ;;
 esac   
+
